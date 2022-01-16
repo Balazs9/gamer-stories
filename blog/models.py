f3cc5de447667a4b708e054e3f3082f5c906f7eb
Balazs9 <sempergerbalazs@gmail.com>
2022-01-16 18:46:21 +0000

    create superuser

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Drafts"), (1, "Published"))
+
+
+class PostStories(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    posted_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['posted_date']
+
+    def __str__(self):
+        return self.title
+
