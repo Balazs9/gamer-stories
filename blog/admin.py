7b4c980b9624b050b60d1b715f3dc200b074da7e
Balazs9 <sempergerbalazs@gmail.com>
2022-01-16 20:52:49 +0000

    add storie, comment to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Storie, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Storie)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'posted_date')
+    search = ('title', 'content')
+    list_filter = ('status', 'posted_date')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'textbox', 'post', 'posted_date', 'approved')
+    list_filter = ('posted_date', 'approved')
+    search = ('name', 'email', 'textbox')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -5,15 +5,39 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Drafts"), (1, "Published"))
 
 
-class PostStories(models.Model):
+class Storie(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    posted_date = models.DateTimeField(auto_now=True)
+    posted_image = CloudinaryField('image', default='placeholder')
+    posted_date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='bloglike', blank=True)
     
     class Meta:
         ordering = ['posted_date']
 
     def __str__(self):
         return self.title
+
+    def gamers_like(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Storie, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    textbox = models.TextField()
+    posted_date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['posted_date']
+
+    def __str__(self):
+        return f"{self.name} commented the following: {self.textbox}"
+
 
