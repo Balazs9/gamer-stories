128e942b425835c4b69b778e74decdd6bc88d20b
Balazs9 <sempergerbalazs@gmail.com>
2022-01-19 21:06:46 +0000

    add StorieList view to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Storie
 
-# Create your views here.
+
+class StorieList(generic.ListView):
+    model = Storie
+    queryset = Storie.objects.filter(status=1).order_by('posted_on')
+    template_name = 'index.html'
+    paginate_by = 8
+
