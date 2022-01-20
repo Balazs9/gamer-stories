9fc35ed6b2c0318ec9996d42c91d50d58488fc72
Balazs9 <sempergerbalazs@gmail.com>
2022-01-20 19:07:30 +0000

    add pagination to posts and implement index.html and style.css

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Storie
 
 class StorieList(generic.ListView):
     model = Storie
-    queryset = Storie.objects.filter(status=1).order_by('posted_on')
+    queryset = Storie.objects.filter(status=1).order_by('posted_date')
     template_name = 'index.html'
     paginate_by = 8
 
