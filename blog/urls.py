aac8eaca40d2941f7bd1292798afd6aaf7304e78
Balazs9 <sempergerbalazs@gmail.com>
2022-01-20 22:50:47 +0000

    implement comment.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.StorieList.as_view(), name='home')
+    path('', views.StorieList.as_view(), name='home'),
+    path('<slug:slug>/', views.StorieDetail.as_view(), name='comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Storie
 
 
@@ -9,3 +9,14 @@ class StorieList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 8
 
+
+class StorieDetail(View):
+    template_name = 'comment.html'
+
+    def get_comment(self, request, *args, **kwargs):
+        queryset = Storie.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('posted_date')
+        
+        return render(request, self.template_name, comments)
+        
