a26d990e35a9d5e61e0038ffb5ffe13a46b7f233
Balazs9 <sempergerbalazs@gmail.com>
2022-01-22 20:15:37 +0000

    add comment box and commenting

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Storie
-
+from .forms import CommentForm
 
 
 class StorieList(generic.ListView):
@@ -18,10 +18,40 @@ class StorieDetail(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('posted_date')
         
-        return render(request, "comment_detail.html", {"post": post, "comments": comments})
+        return render(
+            request,
+            "comment_detail.html",
+            {
+                "post": post, 
+                "comments": comments, 
+                "commented": False,
+                "comment_form": CommentForm()
+            },
+        )
 
-
-
-   
-
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Storie.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('posted_date')
         
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "comment_detail.html",
+            {
+                "post": post, 
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form
+            },
+        )
